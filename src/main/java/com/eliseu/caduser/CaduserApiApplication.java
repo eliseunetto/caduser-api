e34fd7435d6cc080784aa44029f3be04cf928e69
eliseunetto <eliseu.netto@gmail.com>
2021-01-25 19:13:43 -0300

    Add files via upload

---
+++ src/main/java/com/eliseu/caduser/CaduserApiApplication.java
@@ -0,0 +1,13 @@
+package com.eliseu.caduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaduserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaduserApiApplication.class, args);
+	}
+
+}
